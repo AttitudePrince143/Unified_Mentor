a430523980b520d69bebddc8c84e826a0da36604
AttitudePrince143 <sayyedadil22666@gmail.com>
2025-08-01 15:59:03 +0530

    mmmmmv

--- Multi-Theme-Switcher-App/vite.config.ts
+++ Multi-Theme-Switcher-App/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'; // 👈 this line is missing in your error
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Unified_Mentor/Multi-Theme-Switcher-App/', // or '/Multi_Theme_App/' for direct repo
+  base: '/Unified_Mentor/Multi-Theme-Switcher-App/', // 👈 this is important
   plugins: [react()],
 });
