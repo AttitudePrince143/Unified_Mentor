b8331be679bf61d2cfa6f2842eefee2eb2d0187b
AttitudePrince143 <sayyedadil22666@gmail.com>
2025-08-01 17:22:23 +0530

    dlted

--- Multi-Theme-Switcher-App/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/Unified_Mentor/Multi-Theme-Switcher-App/', // 👈 must match subfolder path in repo
-  plugins: [react()],
-});
